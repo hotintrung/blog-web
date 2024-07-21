066ffd4908956a38baeb18f135e6a45fcc562cb6
hotintrung <hotintrung@gmsil.com>
2024-07-21 20:48:38 +0700

    add degree for education

--- pages/education/index.js
+++ pages/education/index.js
@@ -2,8 +2,9 @@ import React, { useEffect } from "react";
 import { useTheme } from "next-themes";
 import ProjectResume from "../../components/ProjectResume";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchEducation } from "../../store/education/slice";
+import { fetchEducation, getFileById } from "../../store/education/slice";
 import LoadingSpinner from "../../components/Loading";
+import Image from "next/image";
 
 const Education = () => {
   const theme = useTheme();
@@ -16,6 +17,29 @@ const Education = () => {
       dispatch(fetchEducation())
     }
   }, [dispatch, education])
+
+  const getFileIcon = (fileType) => {
+    switch (fileType) {
+      case 'application/pdf':
+        return <Image src="/images/pdf.png" width={50} height={50} alt="pdf" />
+      case 'application/msword':
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        return <Image src="/images/doc.png" width={50} height={50} alt="doc" />
+      case 'application/vnd.ms-excel':
+      case 'text/csv':
+      case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+        return <Image src="/images/xls.png" width={50} height={50} alt="xls" />
+      case 'application/vnd.ms-powerpoint':
+      case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+        return <Image src="/images/ppt.png" width={50} height={50} alt="ppt" />
+      default:
+        return <Image src="/images/file.png" width={50} height={50} alt="file" />
+    }
+  };
+
+  const onClickFile = (item) => {
+    dispatch(getFileById(item))
+  }
 
   return (
     <div
@@ -52,14 +76,14 @@ const Education = () => {
                 <h1 className="text-2xl font-bold">Education</h1>
                 {education?.educations.map((edu, index) => (
                   <div key={index} className="mt-2">
-                  <h2 className="text-lg">{edu?.universityName}</h2>
-                  <h3 className="text-sm opacity-75">
-                    {edu?.universityDate}
-                  </h3>
-                  <p className="text-sm mt-2 opacity-50">
-                    {edu?.universityPara}
-                  </p>
-                </div>
+                    <h2 className="text-lg">{edu?.universityName}</h2>
+                    <h3 className="text-sm opacity-75">
+                      {edu?.universityDate}
+                    </h3>
+                    <p className="text-sm mt-2 opacity-50">
+                      {edu?.universityPara}
+                    </p>
+                  </div>
                 ))}
               </div>
               <div className="mt-5">
@@ -67,7 +91,7 @@ const Education = () => {
                 <div className="flex mob:flex-col desktop:flex-row justify-between">
                   {education.skills && (
                     <div className="mt-2 mob:mt-5">
-                      <ul className="list-disc">
+                      <ul className="list-disc grid laptop:grid-cols-3 tablet:grid-cols-3 mob:grid-cols-1 gap-2">
                         {education.skills.map((skill, index) => (
                           <li key={index} className="ml-5 py-2">
                             {skill?.name}
@@ -76,32 +100,22 @@ const Education = () => {
                       </ul>
                     </div>
                   )}
-
-                  {education.frameworks && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Frameworks</h2>
-                      <ul className="list-disc">
-                        {education.frameworks.map((framework, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {framework}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-
-                  {education.others && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Others</h2>
-                      <ul className="list-disc">
-                        {education.others.map((other, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {other}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
+                </div>
+                <div className="mt-5">
+                  <h1 className="text-2xl font-bold mb-5">Degrees</h1>
+                  <div className="grid laptop:grid-cols-2 tablet:grid-cols-2 mob:grid-cols-1 gap-4">
+                    {education.degrees.map((item, index) => (
+                      <div key={index} onClick={() => onClickFile(item)} className="p-4 rounded-lg shadow">
+                        <div className="flex flex-row">
+                          <div className="w-16">{getFileIcon(item.contentType)}</div>
+                          <div className="w-96 ml-2">
+                            <h2>{item.name}</h2>
+                            <h3 className="text-sm opacity-50 ">{item.description}</h3>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
--- store/education/slice.js
+++ store/education/slice.js
@@ -9,14 +9,38 @@ export const fetchEducation = createAsyncThunk('education/fetchEducation', async
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log(data);
         return data?.data;
     } catch (error) {
         throw new Error('Something went wrong. Please try again!!');
     }
-   
-        
+});
 
+export const getFileById = createAsyncThunk('education/getFileById', async (item) => {
+    try {
+
+        const response = await fetch(`${API_URL}api/web/file-multi/${item.fileId}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json()
+        const { url } = data?.data
+        const fetchContentFile = await fetch(url);
+        if (!fetchContentFile.ok) {
+            throw new Error('Network fetchContentFile was not ok');
+        }
+        const blob = await fetchContentFile.blob();
+        const blobUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = blobUrl;
+        link.download = item.fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(blobUrl);
+        return response;
+    } catch (error) {
+        throw new Error('Something went wrong. Please try again!!');
+    }
 });
 
 const initialState = {
